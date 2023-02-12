c05d93fe88da4a9cf502f5b428333144b236be2e
Ninjdai <ChickenTendo3@gmail.com>
2023-02-12 15:43:01 +0100

    Base bot preparation

---
+++ index.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const Sequelize = require('sequelize');
+const { Client, GatewayIntentBits } = require("discord.js");
+const { token, sequelizeCredentials } = require('./config.json');
+const { deploy_commands } = require('./functions.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessages
+    ]
+});
+
+const sequelize = new Sequelize('database', sequelizeCredentials.username, sequelizeCredentials.password, {
+	host: 'localhost',
+	dialect: 'sqlite',
+	logging: false,
+	storage: 'database.sqlite',
+});
+
+const leveldb = sequelize.define('leveling', {
+	name: {//id
+		type: Sequelize.STRING,
+		unique: true,
+	},
+    xp: Sequelize.INTEGER,
+    level: Sequelize.INTEGER
+});
+
+client.database = {
+	sequelize: sequelize,
+	leveldb: leveldb,
+};
+
+leveldb.sync();
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
+}
+
+deploy_commands(client, true);//true: will refresh slash commands
+
