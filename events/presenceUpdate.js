275a72cb5d58ae536f01313aa4d6d7b40ae372f9
Ninjdai <ChickenTendo3@gmail.com>
2023-02-14 15:45:43 +0100

    Removed spammy dms

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -1,4 +1,3 @@
-const { EmbedBuilder } = require('discord.js');
 const { guildId, supportRoleId } = require('../config.json')
 
 module.exports = {
@@ -9,21 +8,12 @@ module.exports = {
             const role = await newPresence.guild.roles.fetch(supportRoleId);
             if(!newPresence.member.roles.cache.has(role.id)){
                 await newPresence.member.roles.add(role);
-                await newPresence.member.send({ embeds: [thanksEmbed] });
             }
         } else if(newPresence.guild.id==guildId && !newPresence.status =='offline' && oldPresence?.activities[0]?.state?.includes('.gg/paradis') && !newPresence?.activities[0]?.state?.includes('.gg/paradis')){
             const role = await newPresence.guild.roles.fetch(supportRoleId);
             if(newPresence.member.roles.cache.has(role.id)){
                 await newPresence.member.roles.remove(role);
-                await newPresence.member.send({ embeds: [thanksEndEmbed] });
             }
         }
     }
 }
-
-const thanksEmbed = new EmbedBuilder()
-    .setDescription("Merci de soutenir Paradis ! En guise de remerciement, vous avez reçu le rôle soutien !")
-    .setColor("Random")
-const thanksEndEmbed = new EmbedBuilder()
-    .setDescription("Merci d'avoir soutenu Paradis ! Le rôle soutien vous a été retiré car votre statut a changé.")
-    .setColor("Random")
