7d7b4ac0e6a8c6bec51b0215933756f09cd857d6
Ninjdai <65647523+Ninjdai1@users.noreply.github.com>
2023-03-28 20:42:30 +0200

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -8,53 +8,21 @@ module.exports = {
     async execute(message, client) {
         if(message.author.bot) return;
         if(!client.isXPEnabled) return;
-        if(message.content=="+luck") {
-            const now = Date.now();
-            let userData = await client.database.userdb.findOne({ where: { name: message.author.id } });
-            if(userData){
-                if(now - userData.dataValues.guessCD < 24 * 60 * 60 * 1000) return message.reply({ content: "Vous avez déjà utilisé la commande dans les dernières 24H !", ephemeral: true });
-                else await client.database.userdb.update(
-                    { guessCD: now },
-                    { where: { name: message.author.id } }
-                );
+        if(message.content.startsWith("+")) {
+            const cmdName = message.content.replace("+","");
+            const command = client.textcommands.get(cmdName);
+            if (!command) return;
+            
+            try {
+                await command.execute(message, client);
+            } catch (error) {
+                console.error(error);
+                try {
+                    await message.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+                } catch (error){
+                    console.error(error);
+                }
             }
-            else{
-                await client.database.userdb.create({
-                    name: message.author.id,
-                    confessBL: false,
-                    guessCD: now
-                });
-            };
-
-
-            let serverData = await client.database.serverdb.findOne({ where: { name: message.guild.id } });
-            if(!serverData){
-                await client.database.serverdb.create({
-                    name: message.guild.id,
-                    guessReward: null,
-                    guessNum: null,
-                });
-                serverData = await client.database.serverdb.findOne({ where: { name: message.guild.id } });
-            };
-
-            const guessedNum = Math.floor(Math.random() * 9999);
-            const embed = new EmbedBuilder()
-            if(serverData.dataValues.guessNum == guessedNum){
-                embed
-                    .setDescription(
-                        `***Voyons voir si tu as de la chance !*** <:emoji_403:1079866988857409656> <@${message.author.id}> \n\n`
-                        + `Tu as obtenu **${guessedNum}** !\n\n`
-                        + `Tu as obtenu le bon nombre ! Contacte l'équipe pour recevoir ton ${serverData.dataValues.guessReward} !`
-                    )
-            } else {
-                embed
-                    .setDescription(
-                        `***Voyons voir si tu as de la chance !*** <:emoji_403:1079866988857409656> <@${message.author.id}> \n\n`
-                        + `Tu as eu **${guessedNum}** !\n\n`
-                        + `*Si tu obtiens le nombre* ***__${serverData.dataValues.guessNum}__*** *tu gagnes un* ***${serverData.dataValues.guessReward}*** <a:P_boost:972371295771701308>`
-                    )
-            }
-            await message.reply({ embeds: [embed] });
         }
 
         if(message.guildId == guildId) {
